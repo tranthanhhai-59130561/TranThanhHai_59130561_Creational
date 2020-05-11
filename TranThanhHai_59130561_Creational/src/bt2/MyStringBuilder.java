d2a003c4205cfd69e3c6c1fdc64842332186e8e5
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 22:20:47 +0700

    BaiTap2_Creational

--- TranThanhHai_59130561_Creational/src/bt2/MyStringBuilder.java
+++ TranThanhHai_59130561_Creational/src/bt2/MyStringBuilder.java
@@ -10,5 +10,39 @@ package bt2;
  * @author Admin
  */
 public class MyStringBuilder {
+    String str;
+
+    // constructor
+    public MyStringBuilder(String str) {
+        this.str = str;
+    }
+   
+    //Phương thức addString(String s):
+    //Thêm chuỗi s vào sau chuỗi str và trả về kiểu MyStringBuilder.
+    public MyStringBuilder addString(String s) {
+        str += s;
+        return this;
+    }
     
+    //Phương thức addFloat(float f): 
+    //Thêm số s vào sau chuỗi str và trả về kiểu MyStringBuilder.
+    public MyStringBuilder addFloat(float f) {
+        str += f;
+        return this;
+    }
+    
+    //Phương thức addBool(boolean b): 
+    //Thêm giá trị b vào sau chuỗi str và trả về kiểu MyStringBuilder.
+    public MyStringBuilder addBool(boolean b) {
+        str += b;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+    
+    
+
 }
