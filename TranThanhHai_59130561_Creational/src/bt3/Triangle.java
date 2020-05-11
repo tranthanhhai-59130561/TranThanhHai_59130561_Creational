9af6c86b846b762685e3dd2f5b3968b93063d7e6
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 23:29:50 +0700

    BaiTap3_Creational

--- TranThanhHai_59130561_Creational/src/bt3/Triangle.java
+++ TranThanhHai_59130561_Creational/src/bt3/Triangle.java
@@ -10,7 +10,19 @@ package bt3;
  * @author Admin
  */
 public class Triangle extends Shape {
-
+    private static Shape triangle;
+    
+    protected Triangle() {
+        super("Triangle-brush", "Triangle-paper", "Triangle-frame");
+    };
+    
+    public static Shape createTriangle() {
+        if (triangle == null) {
+            triangle = new Triangle();
+        }
+        return triangle;
+    }
+    
     @Override
     public String draw() {
         return "Draw triangle: " + getBrush() + ", " + getPaper() + ", " + getFrame();
