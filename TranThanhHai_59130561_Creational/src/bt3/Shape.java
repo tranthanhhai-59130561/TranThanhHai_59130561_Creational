d99776060c6a44598a102256a1476c83bd7a8cf6
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 22:52:31 +0700

    BaiTap3_Creational

--- TranThanhHai_59130561_Creational/src/bt3/Shape.java
+++ TranThanhHai_59130561_Creational/src/bt3/Shape.java
@@ -9,12 +9,45 @@ package bt3;
  *
  * @author Admin
  */
-public class Shape {
+public abstract class Shape {
     private String brush;
     private String paper;
     private String frame;
     
     protected Shape()
     {}
+
+    // constructor
+    protected Shape(String brush, String paper, String frame) {
+        this.brush = brush;
+        this.paper = paper;
+        this.frame = frame;
+    }
+
+    public String getBrush() {
+        return brush;
+    }
+
+    public void setBrush(String brush) {
+        this.brush = brush;
+    }
+
+    public String getPaper() {
+        return paper;
+    }
+
+    public void setPaper(String paper) {
+        this.paper = paper;
+    }
+
+    public String getFrame() {
+        return frame;
+    }
+
+    public void setFrame(String frame) {
+        this.frame = frame;
+    }
+    
+    public abstract String draw();
             
 }
