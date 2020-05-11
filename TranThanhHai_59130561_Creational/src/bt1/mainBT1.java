2b1b8b80a6c6d75a9a0b2aa7a1927b925016e85c
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 21:29:27 +0700

    BaiTap1_Creational

--- TranThanhHai_59130561_Creational/src/bt1/mainBT1.java
+++ TranThanhHai_59130561_Creational/src/bt1/mainBT1.java
@@ -6,42 +6,30 @@
 package bt1;
 // Alt + Enter
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+
 
 /**
  *
  * @author Admin
  */
-public class mainBT1 {
-    
-    public static LocalDate getDateFromString(String string, DateTimeFormatter format) { // tham khao tren GeeksforGeeks 
-        // Convert the String to Date in the specified format 
-        LocalDate date = LocalDate.parse(string, format); 
-        // return the converted date 
-        return date; 
-    } 
+public class mainBT1 {   
+     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ParseException {
         // TODO code application logic here
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
         
-        HoaDonHeader hdh = new HoaDonHeader("HD01", getDateFromString("31/3/1999", format),"Trần Thanh Hải");
-        CTHD cthd1 = new CTHD("Ram 32 GB", 1, 85000, 0.5);
-        CTHD cthd2 = new CTHD("Ram 8 GB", 2, 100000, 0.8);
-        CTHD cthd3 = new CTHD("Ram 4 GB ", 4, 150000, 0.8);
-        CTHD cthd4 = new CTHD("Ram 16 GB", 2, 140000, 1.4);
-        
-        HoaDon hd = new HoaDon.Builder()
-                .addHoaDonHeader(hdh)
-                .addCTHD(cthd1)
-                .addCTHD(cthd2)
-                .addCTHD(cthd3)
-                .addCTHD(cthd4)
-                .build();
-       System.out.println(hd.toString());
+        HoaDon hoaDon = new HoaDon.Builder()
+                            .addHoaDonHeader(new HoaDonHeader("HD01", formatDate.parse("31/3/1999"), "Trần Thanh Hải"))
+                            .addCTHD(new CTHD("Ram 16 GB", 2, 1800000, 12))
+                            .addCTHD(new CTHD("Ram 4 GB", 4, 1700000, 15))
+                            .addCTHD(new CTHD("Ram 32 GB", 1, 3500000, 12))
+                            .addCTHD(new CTHD("Ram 8 GB", 3, 2300000, 10))
+                            .build();
+        System.out.println(hoaDon.toString());
     }
-    
+       
 }
