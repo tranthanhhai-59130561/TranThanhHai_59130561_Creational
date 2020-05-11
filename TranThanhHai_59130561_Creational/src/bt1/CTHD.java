7a0302c4638da2523ef5267267644ca9ca213f16
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 19:12:18 +0700

    BaiTap1_Creational

--- TranThanhHai_59130561_Creational/src/bt1/CTHD.java
+++ TranThanhHai_59130561_Creational/src/bt1/CTHD.java
@@ -10,5 +10,52 @@ package bt1;
  * @author Admin
  */
 public class CTHD {
+    String sanPham;
+    int soLuong;
+    double donGia;
+    float chiecKhau;
+    
+    // constructor
+    public CTHD(String sanPham, int soLuong, double donGia, float chiecKhau) {
+        this.sanPham = sanPham;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.chiecKhau = chiecKhau;
+        
+        
+    }
+    // getter/setter
+    public String getSanPham() {
+        return sanPham;
+    }
+
+    public void setSanPham(String sanPham) {
+        this.sanPham = sanPham;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public float getChiecKhau() {
+        return chiecKhau;
+    }
+
+    public void setChiecKhau(float chiecKhau) {
+        this.chiecKhau = chiecKhau;
+    }
+    
     
 }
