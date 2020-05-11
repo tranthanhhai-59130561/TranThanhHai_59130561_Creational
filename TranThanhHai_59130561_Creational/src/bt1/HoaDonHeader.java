bec579f425f23ab0a4c7d49661ce6ec55c86498c
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 18:47:38 +0700

    BaiTap1_Creational

--- TranThanhHai_59130561_Creational/src/bt1/HoaDonHeader.java
+++ TranThanhHai_59130561_Creational/src/bt1/HoaDonHeader.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package bt1;
-
+import java.time.LocalDate;
 /**
  *
  * @author Admin
  */
 public class HoaDonHeader {
+    String maHoaDon;
+    LocalDate ngayBan;
+    String tenKhachHang;
+    
+    public HoaDonHeader()
+    {}
+       
+    // Constructor
+    public HoaDonHeader(String maHoaDon, LocalDate ngayBan, String tenKhachHang) 
+    {
+        this.maHoaDon = maHoaDon;
+        this.ngayBan = ngayBan;
+        this.tenKhachHang = tenKhachHang;
+    }
+    
+    
+    
     
 }
