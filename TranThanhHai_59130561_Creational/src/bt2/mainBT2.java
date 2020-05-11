bdd409e4f295682b1642444cf0b44286b0b3b4af
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 22:36:16 +0700

    BaiTap2_Creational

--- TranThanhHai_59130561_Creational/src/bt2/mainBT2.java
+++ TranThanhHai_59130561_Creational/src/bt2/mainBT2.java
@@ -16,10 +16,11 @@ public class mainBT2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        MyStringBuilder str = new MyStringBuilder()
-                .addString("Trần Thanh Hải")
-                .addString("59130561")
+        MyStringBuilder str = new MyStringBuilder("- ")
+                .addString("Trần Thanh Hải | ")
+                .addString("59130561 | Điểm: ")
                 .addFloat(7.0f)
+                .addString(" | Môn MTK: ")
                 .addBool(true);
         System.out.println(str.toString());               
     }
