10113a72d2bc8f653c0b994fc70527b3ac71dc47
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 20:58:02 +0700

    BaiTap1_Creational

--- TranThanhHai_59130561_Creational/src/bt1/mainBT1.java
+++ TranThanhHai_59130561_Creational/src/bt1/mainBT1.java
@@ -4,16 +4,44 @@
  * and open the template in the editor.
  */
 package bt1;
+// Alt + Enter
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author Admin
  */
 public class mainBT1 {
+    
+    public static LocalDate getDateFromString(String string, DateTimeFormatter format) { // tham khao tren GeeksforGeeks 
+        // Convert the String to Date in the specified format 
+        LocalDate date = LocalDate.parse(string, format); 
+        // return the converted date 
+        return date; 
+    } 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+        HoaDonHeader hdh = new HoaDonHeader("HD01", getDateFromString("31/3/1999", format),"Trần Thanh Hải");
+        CTHD cthd1 = new CTHD("Ram 32 GB", 1, 85000, 0.5);
+        CTHD cthd2 = new CTHD("Ram 8 GB", 2, 100000, 0.8);
+        CTHD cthd3 = new CTHD("Ram 4 GB ", 4, 150000, 0.8);
+        CTHD cthd4 = new CTHD("Ram 16 GB", 2, 140000, 1.4);
+        
+        HoaDon hd = new HoaDon.Builder()
+                .addHoaDonHeader(hdh)
+                .addCTHD(cthd1)
+                .addCTHD(cthd2)
+                .addCTHD(cthd3)
+                .addCTHD(cthd4)
+                .build();
+       System.out.println(hd.toString());
     }
+    
 }
