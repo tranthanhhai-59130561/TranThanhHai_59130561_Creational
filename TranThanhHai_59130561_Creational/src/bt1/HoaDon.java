da137ebc07079c0f875a82ae0da42cb162d1e08d
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-05-11 20:35:24 +0700

    BaiTap1_Creational

--- TranThanhHai_59130561_Creational/src/bt1/HoaDon.java
+++ TranThanhHai_59130561_Creational/src/bt1/HoaDon.java
@@ -6,16 +6,52 @@
 package bt1;
 import java.util.ArrayList;
 import java.time.LocalDate;
+
 /**
  *
  * @author Admin
  */
 public class HoaDon {
     HoaDonHeader hoaDonHeader = new HoaDonHeader();
-    ArrayList<CTHD> cthd = new ArrayList<>();
+    ArrayList<CTHD> dsCT = new ArrayList<>();
+    
+    public HoaDon(Builder builder) // public static class Builder{} 
+    {
+        this.hoaDonHeader = builder.hoaDonHeader;
+        this.dsCT = builder.dsCT ;
+    }
+    
     public static class Builder{
         HoaDonHeader hoaDonHeader;
-        ArrayList<CTHD> cthd = new ArrayList<>();
+        ArrayList<CTHD> dsCT = new ArrayList<>();
+          
+        public Builder()
+        {}
+        
+        public Builder addHoaDonHeader(HoaDonHeader hoaDonHeader) {
+            this.hoaDonHeader = hoaDonHeader;
+            return this;
+        }
+        
+        public Builder addCTHD(CTHD cthd){
+            this.dsCT.add(cthd);
+            return this;
+        }
+        
+        public HoaDon build(){
+            return new HoaDon(this);
+        }
+
+        //toString
+        @Override
+        public String toString() {
+            String s = hoaDonHeader.toString();
+            for(int i=0; i<dsCT.size(); i++){
+            s += "\n"+dsCT.get(i).toString();
+            }
+            return s;
+        }
+                 
     }
     
 }
